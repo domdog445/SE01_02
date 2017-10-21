d4215a88cb825cd80057564ca8216a0dfcb646d1
domdog445 <32840794+domdog445@users.noreply.github.com>
2017-10-21 19:52:07 +0200

    Version 1

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -1,15 +1,85 @@
-
-
+import java.util.LinkedList;
+import java.util.List;
 
 public class Project {
+	// Hilfsklasse
+	class Rule {
+		Rule(String newname) {
+			prozessname = newname;
+		}
 
-    private final String[][] constraints;
+		List<Rule> Bedingungen = new LinkedList<Rule>();
+		String prozessname;
+		boolean checked = false;
+	}
 
-    public Project(String[][] constraints) {
-        this.constraints = constraints;
-    }
+	List<Rule> regeln = new LinkedList<Rule>();
 
-    public boolean isWellSorted(String[] sequence) {
-        throw new UnsupportedOperationException("Nicht implementiert!");
-    }
+	// Konstruktor
+	public Project(String[][] newregeln) {
+		super();
+		// Regeln parsen
+		for (int i = 0; i < newregeln.length; i++) {
+			Rule newprozess;
+			if (existprozess(newregeln[i][1]) == false) {
+				newprozess = new Rule(newregeln[i][1]);
+				regeln.add(newprozess);
+			} else {
+				newprozess = getrulebyname(newregeln[i][1]);
+			}
+			Rule newrequirements;
+			if (existprozess(newregeln[i][0]) == false) {
+				newrequirements = new Rule(newregeln[i][0]);
+				regeln.add(newrequirements);
+			} else {
+				newrequirements = getrulebyname(newregeln[i][0]);
+			}
+			newprozess.Bedingungen.add(newrequirements);
+		}
+	}
+
+	// Existiert der Prozess
+	private boolean existprozess(String prozessname) {
+		for (Rule tmp : regeln) {
+			if (tmp.prozessname.equals(prozessname)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Wirft den Prozess zurück
+	private Rule getrulebyname(String prozessname) {
+		for (Rule tmp : regeln) {
+			if (tmp.prozessname.equals(prozessname)) {
+				return tmp;
+			}
+		}
+		return null;
+	}
+
+	public boolean isWellSorted(String[] sequence) { 
+		 //Reset checked Status
+		rulesreset(); 
+		for(int i = 0; i< sequence.length;i++)
+		{ 
+			Rule testrule = getrulebyname(sequence[i]);
+			//Sind alle Vorbedingunen erledigt 
+			for(Rule tmprequirements : testrule.Bedingungen)
+			{
+				if(tmprequirements.checked == false) 
+				{ 
+					return false; 
+				} 
+			}
+			testrule.checked = true; 
+			} 
+		return true; 
+	} // Reset Status
+
+	private void rulesreset() {
+		for (Rule tmpregel : regeln) {
+			tmpregel.checked = false;
+		}
+	}
 }
